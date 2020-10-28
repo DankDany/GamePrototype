50fb40a49b8c7d13151420fab00d00b7fb6ba74f
DankDany <71140298+DankDany@users.noreply.github.com>
2020-10-28 11:54:59 -0500

    Movement / Mouse Tracing
    Made the movement to the arrow keys and the character is following the mouse wherever it goes

---
+++ GamePrototype/Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 2f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += Time.deltaTime * _moveSpeed * Vector3.right;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position += Time.deltaTime * _moveSpeed * Vector3.left;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.position += Time.deltaTime * _moveSpeed * Vector3.forward;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.position += Time.deltaTime * _moveSpeed * Vector3.back;
+    }
+}
---
+++ GamePrototype/Assets/RotationScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Lookatmouse();
+        
+    }
+    void Lookatmouse()
+    {
+        Plane playerplane = new Plane(Vector3.up, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float hitdist;
+
+        if (playerplane.Raycast(ray, out hitdist))
+        {
+            Vector3 targetpoint = ray.GetPoint(hitdist);
+            Quaternion targetrotation = Quaternion.LookRotation(targetpoint - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetrotation, speed * Time.deltaTime);
+        }
+    }
+}
